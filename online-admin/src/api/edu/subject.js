8081666e09dc1e4fc747f98d2827123a8b9bffb8
张淞淇 <z2474942479@163.com>
2021-05-17 12:59:48 +0800

    课程分类重构

--- online-admin/src/api/edu/subject.js
+++ online-admin/src/api/edu/subject.js
@@ -4,12 +4,34 @@ export default {
 
 
     //查询所有的课程分类列表
-    getAllList(){
+    getAllList() {
         return request({
             url: '/eduService/subject/getAllSubject',
             method: 'get',
-          })
+        })
+    },
+
+    getSubjectInfo(id) {
+        return request({
+            url: '/eduService/subject/getSubjectInfo/' + id,
+            method: 'get'
+        })
+    },
+
+    saveOrUpdateSubject(subjectDTO) {
+        return request({
+            url: '/eduService/subject/saveOrUpdateSubject',
+            method: 'post',
+            data: subjectDTO
+        })
+    },
+
+    removeSubject(id) {
+        return request({
+            url: '/eduService/subject/deleteSubject/' + id,
+            method: 'delete'
+        })
     }
 
-  
+
 }
